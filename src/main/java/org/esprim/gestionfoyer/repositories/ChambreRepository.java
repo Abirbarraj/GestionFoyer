2cf1e3bec355a7adec9de2b2d95e26e8425196e0
Abir barraj <abirbarraj224@gmail.com>
2025-10-24 23:47:01 +0100

    3 commit

--- src/main/java/org/esprim/gestionfoyer/repositories/ChambreRepository.java
+++ src/main/java/org/esprim/gestionfoyer/repositories/ChambreRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChambreRepsoitory extends JpaRepository<Chambre,Long> {
+public interface ChambreRepository extends JpaRepository<Chambre,Long> {
 }
--- src/main/java/org/esprim/gestionfoyer/service/ChambreServiceImpl.java
+++ src/main/java/org/esprim/gestionfoyer/service/ChambreServiceImpl.java
@@ -1,8 +1,8 @@
 package org.esprim.gestionfoyer.service;
 
-import lombok.AllArgsConstructor;
+
 import org.esprim.gestionfoyer.entities.Chambre;
-import org.esprim.gestionfoyer.repositories.ChambreRepsoitory;
+import org.esprim.gestionfoyer.repositories.ChambreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,26 +13,26 @@ import java.util.List;
 public class ChambreServiceImpl implements IChambreService {
 
     @Autowired
-    ChambreRepsoitory ChambreRepsoitory;
+    ChambreRepository chambreRepository;
 
     @Override
     public List<Chambre> retrieveAllChambres() {
-        return ChambreRepsoitory.findAll();
+        return chambreRepository.findAll();
     }
     @Override
     public Chambre retrieveChambre(Long chambreId) {
-        return ChambreRepsoitory.findById(chambreId).get();
+        return chambreRepository.findById(chambreId).get();
     }
     @Override
     public Chambre addChambre(Chambre c) {
-        return ChambreRepsoitory.save(c);
+        return chambreRepository.save(c);
     }
     @Override
     public void removeChambre(Long chambreId) {
-        ChambreRepsoitory.deleteById(chambreId);
+        chambreRepository.deleteById(chambreId);
     }
     @Override
     public Chambre modifyChambre(Chambre chambre) {
-        return ChambreRepsoitory.save(chambre);
+        return chambreRepository.save(chambre);
     }
 }
